1e28a9e7e373d39c99790823f26c585e948681ac
cornelius31415 <50702246+cornelius31415@users.noreply.github.com>
2024-09-24 18:26:55 +0200

    Add files via upload

---
+++ ArrayClass.py
@@ -0,0 +1,83 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Sep 24 13:34:50 2024
+
+@author: cornelius
+"""
+
+"""
+                    BASIC OPERATIONS ON DATASTRUCTURES
+            
+            1. INSERT 
+            2. SEARCH
+            3. DELETE
+            4. TRAVERSE
+
+
+                    DIFFERENCES BETWEEN LISTS AND ARRAYS
+                    
+            - items of a list do not all have to be of the same data type
+            - for an array all elements must be of the same type
+            - knowing the type of the items means knowing the amount of memory
+              needed to represent each one
+            - therefore the memory for the entire array can be allocated
+            
+            - arrays are of a fixed size from the start
+            - the compiler needs to know how much space to set aside for the
+              whole array and keep it separate from all the other data
+            
+"""
+
+class Array():
+    
+    def __init__(self,initial_size):   # arrays have a fixed size 
+        self.array = [None]*initial_size # define array as list with None initial_size times
+        self.amount = 0 # amount of elements in array
+        self.initial_type = None # every array contains onyl elements of one data type
+
+
+        
+    def insert(self,item): # insert a new element into the array
+        
+        if self.amount == 0: # if no element is yet in the array 
+            
+            self.initial_type = type(item) # set type of array to type of item
+            self.array[self.amount]=item # insert item at the current end of array
+            self.amount += 1 # increase the amount of items by on
+            
+        elif self.amount > 0:
+            if type(item) != self.initial_type: # check if type of element to be added
+                print("Not the right data type") # is the same as the set initial type of array
+            else:
+                self.array[self.amount]=item # insert item at the current end of array
+                self.amount += 1 # increase the amount of items by one 
+
+
+    
+    def search(self,item):
+        for i in range(self.amount):
+            if self.array[i] == item: # if found
+                return self.array[i] # return item
+        
+        
+        
+    def delete(self,item):
+        for i in range(self.amount):
+            if self.array[i] == item: # if item found
+                for k in range(i,self.amount): # go through elements with higher indizes
+                    self.array[k] = self.array[k+1] # and shift them one down
+                self.amount -= 1    
+                
+    
+    
+    def traverse(self): # print each element of the array
+        for i in range(self.amount):
+            print(self.array[i])
+    
+ar = Array(2)
+
+ar.insert(2)
+ar.traverse()
+ar.insert(10)
+ar.traverse()
