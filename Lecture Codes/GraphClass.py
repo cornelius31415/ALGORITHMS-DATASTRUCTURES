8fb591a0a2c8617bfaf6bdf2a7a9d56e1180ef8c
cornelius31415 <50702246+cornelius31415@users.noreply.github.com>
2024-10-04 18:58:54 +0200

    Add files via upload

--- Lecture Codes/GraphClass.py
+++ Lecture Codes/GraphClass.py
@@ -7,6 +7,7 @@ Created on Tue Oct  1 11:42:01 2024
 """
 import numpy as np
 import matplotlib.pyplot as plt
+import math
 
 class Graph():
     
@@ -29,10 +30,10 @@ class Graph():
                 
      
     
-    def add_edge(self,vertex1, vertex2):
+    def add_edge(self,vertex1, vertex2, weight = 1):
         
-        self.adjacency_matrix[vertex1,vertex2] = 1
-        self.adjacency_matrix[vertex2,vertex1] = 1
+        self.adjacency_matrix[vertex1,vertex2] = weight
+        self.adjacency_matrix[vertex2,vertex1] = weight
 
     # -----------------------------------------------------------------------------
     #                               ANALYZING A GRAPH
@@ -41,10 +42,89 @@ class Graph():
     def neighbors(self,vertex):
         neighbors = []
         for node in self.nodes:
-            if node != vertex and self.adjacency_matrix[node,vertex]==1:
+            if node != vertex and self.adjacency_matrix[node,vertex] != 0:
                 neighbors.append(node)
         
         return neighbors
+    
+
+
+    
+    
+    def dijkstra(self,start,end):
+        
+        """
+                                        1.
+        - Create a dictionary for the cost to get to each node from the start
+        - Set cost to reach start to 0
+        - Set cost to reach every other node to infinity
+        
+                                        2.
+        - Create a list for each node visited
+        - A node will be added after it has been chosen as the one neighbor
+          whose overall cost is minimal
+        
+                                        3.
+        - Set current node to start 
+        - Get neighbors of this current node
+        
+                                        4.
+        - Calculate the cost of getting to each neighbor node by getting
+          the cost to get to the current node from the costs-dictionary
+          and add it to the weight of the edge 
+          
+                                        5.
+        - If the calculated cost is smaller than the one written in 
+          the costs-dictionary, then update the cost
+          
+                                        6.
+        - Append the current node to the list of visited nodes
+        
+                                        7.
+        - Get all the neighbors of the current node that have not yet 
+          been added to the visited list
+        - Go through the neighbors, check if they are in the visited list
+          and add the unvisited neighbor as a key to a dictionary whose values
+          will be the cost to reach that neighbor
+          
+                                        8.
+        - Choose the neighbor with the smallest cost
+        - Append end node to the visited list
+        -> Now the visited list is the shortest path
+        
+        
+        """
+        costs = {start: 0}
+        for node in self.nodes:
+            if node != start:
+                costs[node] = np.inf
+    
+        visited = []
+        current_node = start
+        
+        while current_node != end:
+            
+            neighbors = self.neighbors(current_node)
+            
+            for neighbor in neighbors:
+                
+                cost = costs[current_node] + self.adjacency_matrix[neighbor,current_node]
+                if cost < costs[neighbor]:
+                    costs[neighbor] = cost
+            visited.append(current_node)
+            
+            # now i need neighbors who have not been visited before and choose the one with
+            # the shortest overall path
+            unvisited_neighbors = {}
+            for neighbor in neighbors:
+                if neighbor not in visited:
+                    unvisited_neighbors[neighbor] = costs[neighbor]
+                
+            current_node = min(unvisited_neighbors,key=unvisited_neighbors.get)
+                            
+        visited.append(end)
+        return visited, costs[end]
+        
     
     # -----------------------------------------------------------------------------
     #                               PLOTTING A GRAPH
@@ -72,12 +152,19 @@ class Graph():
         
         for vertex1 in self.nodes:
             for vertex2 in self.nodes:
-                if self.adjacency_matrix[vertex1,vertex2] == 1:
+                if self.adjacency_matrix[vertex1,vertex2] != 0:
+                    weight = self.adjacency_matrix[vertex1, vertex2]
                     x1,y1 = node_positions[vertex1]
                     x2,y2 = node_positions[vertex2]
-                    ax.plot([x1,x2],[y1,y2],zorder=1)
+                    ax.plot([x1,x2],[y1,y2],zorder=1,color='orange')
          
-                
+                    mid_x, mid_y = (x1+x2)/2, (y1+y2)/2
+                    ax.text(mid_x,mid_y,str(weight),ha='center',va='center',zorder=3,
+                            bbox=dict(facecolor='mintcream', edgecolor='black', 
+                                      boxstyle='round,pad=0.5'))    
+                    
+                    
+                    
 
         
         
@@ -91,11 +178,14 @@ g.add_vertex("A")
 g.add_vertex("B")
 g.add_vertex("C")
 g.add_vertex("D")
-g.add_edge("A", "B")
-g.add_edge("B", "D")
-g.add_edge("A", "C")
-g.add_edge("B", "C")
+g.add_edge("A", "B",100)
+g.add_edge("A", "D",20)
+g.add_edge("A", "C",50)
+g.add_edge("B", "C",10)
+g.add_edge("D", "C",125)
+# print(g.adjacency_matrix)
 print(g.neighbors("D"))
+print(g.dijkstra("D", "B"))
 g.draw_graph()
 
 
